8c82ca59250cf3b341bd2be463758903d553070d
xHavckedx <65540386+xHavckedx@users.noreply.github.com>
2022-03-12 23:15:22 +0100

    UPDATE

--- docs/javascript/app.js
+++ docs/javascript/app.js
@@ -11,3 +11,18 @@ function cerrarMenu(){
         let ico = document.getElementById('navresponsive__ico')
         ico.classList.toggle("fa-x")
 }
+
+function vermas(a){
+    if(a==0){
+        let boton1 = document.getElementById('frontp1')
+        boton1.classList.toggle('habilidades__container-p-open')
+    }
+    if(a==1){
+        let boton2 = document.getElementById('frontp2')
+        boton2.classList.toggle('habilidades__container-p-open')
+    }
+    if(a==2){
+        let boton3 = document.getElementById('frontp3')
+        boton3.classList.toggle('habilidades__container-p-open')
+    }
+}
