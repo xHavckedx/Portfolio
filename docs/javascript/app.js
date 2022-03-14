80f05a4af9f0fbc1d10059e545c14cc3c6538fd9
xHavckedx <65540386+xHavckedx@users.noreply.github.com>
2022-03-14 19:19:14 +0100

    UPDATE

--- docs/javascript/app.js
+++ docs/javascript/app.js
@@ -13,16 +13,35 @@ function cerrarMenu(){
 }
 
 function vermas(a){
+    let texto = document.getElementsByClassName('habilidades__container-divfooter');
     if(a==0){
         let boton1 = document.getElementById('frontp1')
         boton1.classList.toggle('habilidades__container-p-open')
+        if(boton1.classList.contains('habilidades__container-p-open')){
+            texto[0].textContent = 'Mostrar menos'
+        }
+        if(!boton1.classList.contains('habilidades__container-p-open')){
+            texto[0].textContent = 'Ver más'
+        }
     }
     if(a==1){
         let boton2 = document.getElementById('frontp2')
         boton2.classList.toggle('habilidades__container-p-open')
+        if(boton2.classList.contains('habilidades__container-p-open')){
+            texto[1].textContent = 'Mostrar menos'
+        }
+        if(!boton2.classList.contains('habilidades__container-p-open')){
+            texto[1].textContent = 'Ver más'
+        }
     }
     if(a==2){
         let boton3 = document.getElementById('frontp3')
         boton3.classList.toggle('habilidades__container-p-open')
+        if(boton3.classList.contains('habilidades__container-p-open')){
+            texto[2].textContent = 'Mostrar menos'
+        }
+        if(!boton3.classList.contains('habilidades__container-p-open')){
+            texto[2].textContent = 'Ver más'
+        }
     }
 }
